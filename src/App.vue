0ea5944f5ad80aae0a20d551fab040b515da4716
Tu Nombre <tucorreo@ejemplo.com>
2025-06-19 18:22:58 -0500

    primer cambio

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+{{ mensaje }}
+<button type="button" name="cargar" @click="incrementar">
+cargar
+</button>
+</template>
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      mensaje: 0
+    };
+  },
+  methods:{
+    incrementar(){
+      this.mensaje++;
+    }
+  }
+  }
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
